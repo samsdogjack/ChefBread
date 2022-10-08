8775176273786787e2fb0801cd73e4a103076897
samsdogjack <105887707+samsdogjack@users.noreply.github.com>
2022-10-08 18:37:03 -0400

    setup all bakers date

--- server.js
+++ server.js
@@ -33,6 +33,11 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+// bakers 
+const bakersController = require('./controllers/bakers_controller.js')
+app.use('/bakers', bakersController)
+
+
 // 404 Page
 app.get('*', (req, res) => {
   res.send('404')
